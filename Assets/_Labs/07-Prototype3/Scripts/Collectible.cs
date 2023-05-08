dce21a56bae29d74e4ac32943d605ce07d430689
stumpy252 <123128220+stumpy252@users.noreply.github.com>
2023-05-07 22:36:03 -0400

    made some text to add a story

--- Assets/_Labs/07-Prototype3/Scripts/Collectible.cs
+++ Assets/_Labs/07-Prototype3/Scripts/Collectible.cs
@@ -1,15 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class Collectible : MonoBehaviour
-{
-  private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.CompareTag("Player"))
-        {
-            Destroy(this.gameObject);
-            
-        }
+
+public class Collectible : MonoBehaviour{
+    public TextMeshProUGUI countText;
+    public GameObject firstCollectible;
+    private int count;
+    void Start(){
+        firstCollectible.SetActive(false);
+        count = 0;
+        SetCountText();
     }
-}
+    private void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("Player")){
+            count += 1;
+            SetCountText();
+            this.gameObject.SetActive(false); 
+        }
+       
+    }
+    
+    void SetCountText()
+    {
+        countText.text = "Collectibles: " + count.ToString();
+        if(count == 1){
+            firstCollectible.SetActive(true);
+            Invoke("SetFalse", 5.0f);
+        }
+        
+    }
+    void SetFalse(){
+            firstCollectible.SetActive(false);
+        }
+   
+} 
